66b851103834da51e958c77a848eaccca870cb09
limit queen <63425451+LimitQueenProject@users.noreply.github.com>
2020-04-17 17:39:19 +0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -10,17 +10,17 @@ import time
 
 
 print ("\33[31;1m  _____       _____        ______      ________   ")
-time.sleep(0.2)
+time.sleep(0.15)
 print ("\33[31;1m | ___ +,    | ___ +,    /  ____  \   |  ______|  ")
-time.sleep(0.2)
+time.sleep(0.15)
 print ("\33[31;1m ||   \ ++   ||   \ ++  |  |    |  |  | |______   ") 
-time.sleep(0.2)
+time.sleep(0.15)
 print ("\33[31;1m ||    | +   ||    | +  |  |    |  |  |_______ |  ")     
-time.sleep(0.2)
+time.sleep(0.15)
 print ("\33[31;1m ||___/ ++   ||___/ ++  |  |____|  |   ______| |  ")
-time.sleep(0.2)
+time.sleep(0.15)
 print ("\33[31;1m |_____+'    |_____+'    \________/   |________|  ")
-time.sleep(0.2)
+time.sleep(0.15)
 
 print("\33[1;33mTOOLS NYA UDAH GILE")
 
@@ -39,35 +39,6 @@ print ('''
 | Biar bisa di pake harus subs dulu:v              |
 ''')
 time.sleep(0.3)
-
-
-print ("\33[1;33m======================")
-print ("\33[32;1m===MASUKAN PASSWORD=== \33[31;1mkalau g tau ketik apa aja")
-print ("\33[1;33m======================")
-time.sleep(0.2)
-sandi ="start ddos"
-
-for i in range (10000):
-    password = raw_input("\33[31;1mENTER THE PASSWORD:  ")
-    j=10000
-    if (password==sandi):
-        print ("\33[31;1mEKSEKUSI BOSS")
-        time.sleep(0.3)
-        break
-    else:
-        print ("\33[31;1m")
-        print ("Password Salah Silahkan coba lagi" ,j-i)
-        time.sleep(0.2)
-        print ("\33[33;1m\n Subscribe dulu channel Limit Queen Project")
-        time.sleep(0.6)
-        print (" Udh Itu Hubungi WA 089646647191")
-        time.sleep(0.6)
-        print (" Kalo Udah Kasih Bukti, Nanti Bisa Buka Passwordnya")
-        time.sleep(0.6)
-print ("\33[0;32mTOOLS AKAN DI MULAI")
-time.sleep(3)
-print ("LOADING...")
-time.sleep(0.6)
 
        
 # parse input
