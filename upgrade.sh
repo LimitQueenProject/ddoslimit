da9ff018ce1bc85383561d11684e0046e1120310
limit queen <63425451+LimitQueenProject@users.noreply.github.com>
2020-04-17 19:16:15 +0700

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,9 @@
+#!\usr\bin\bash
 cd ..
 pkg update && pkg upgrade
 pkg install git
 rm -rf ddoslimit
 git clone https://github.com/LimitQueenProject/ddoslimit
+cd ddoslimit
+
+echo "upgrade selesai"
